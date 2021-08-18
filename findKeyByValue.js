5f90aabf022c8b71a24b69d6e01208fbade7f274
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-18 22:08:51 +0000

    isolated object keys and initiated loop

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,8 +6,17 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+//get an list of keys
+//loop through key list
+  //determine if key value = input value
+    //return if match
+  //if no match found
+    //return undefined;
+
 const findKeyByValue = function(obj, value){
-  
+  let output = "";
+  const objKeys = Object.keys(obj);
+  console.log("objKeys:", objKeys)
 }
 
 const bestTVShowsByGenre = { 
