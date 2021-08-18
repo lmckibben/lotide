4e05e747cd6b4dde083f395f59e13753915354ae
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-18 21:01:01 +0000

    set up: count letters functoin, test cases

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//create empty object
+//loop through string
+//if then add/count each unique letter in the string adding to a object
+//skip spaces
+
+const countLetters = function(string) {
+  const results = {};
+  
+  for (const letters of string) {
+    if (results[letters]) {
+      results[letters] += 1
+    }else if (letters !== " ") {
+      results[letters] = 1
+    }
+  }
+
+  return results;
+}
+
+
+const result1 = countLetters("this is a test");
+console.log("results1:", result1);
+
+assertEqual(result1["t"], 3);
+assertEqual(result1["h"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["s"], 3);
+assertEqual(result1["a"], 1);
+assertEqual(result1["e"], 1);
+assertEqual(result1[" "], undefined);
