8f8bce17e354f6ad40f697f1a2d94ddcb596350b
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-18 22:31:26 +0000

    checked if value of key matches input value, if yes changed output to match key, checked to see if output.length is === 0 if it is then set output to = undefined

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,22 +8,37 @@ const assertEqual = function(actual, expected) {
 
 //get an list of keys
 //loop through key list
-  //determine if key value = input value
-    //return if match
-  //if no match found
-    //return undefined;
+//determine if key value = input value
+//return if match
+//if no match found
+//return undefined;
 
-const findKeyByValue = function(obj, value){
+const findKeyByValue = function(obj, value) {
   let output = "";
   const objKeys = Object.keys(obj);
-  console.log("objKeys:", objKeys)
-}
+  
+  for (let i = 0; i < objKeys.length; i++) {
+    if (obj[objKeys[i]] === value) {
+      output = objKeys[i];
+    }
+  }
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+  if (output.length === 0) {
+    output = undefined;
+  }
+  return output;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+  drama:  "The Wire",
+  romance: "The Notebook",
+  horror: "Scream",
+  action: "Terminator 2"
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Notebook"), "romance");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Shining"), undefined);
