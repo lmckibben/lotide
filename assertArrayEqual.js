b661f2c01314c14891ec6cf2ee026ea56dffcd7c
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-19 02:12:22 +0000

    copied over fix for eqArray

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,16 +1,18 @@
 const eqArrays = function(arrayA, arrayB) {
   let output = true;
 
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
   for (let i = 0; i < arrayA.length; i++) {
     for (let j = 0; j < arrayB.length; j++) {
-      if (i === j && arrayA[i] !== arrayB[j]){
+      if (i === j && arrayA[i] !== arrayB[j]) {
         output = false;
       }
     }
   }
-  return output
-}
-
+  return output;
+};
 const assertArraysEqual = function(arrayA, arrayB,) {
   if (eqArrays(arrayA, arrayB) === true) {
     console.log(`🟢🟢🟢 Assertion Passed: ${arrayA} === ${arrayB}`);
