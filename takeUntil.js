a92a903774c8727a45b4e70b639a7aa86e86cde0
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-19 21:16:54 +0000

    created takeUntil.js, tests passs

---
+++ takeUntil.js
@@ -0,0 +1,47 @@
+const eqArrays = function(arrayA, arrayB) {
+  let output = true;
+
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    for (let j = 0; j < arrayB.length; j++) {
+      if (i === j && arrayA[i] !== arrayB[j]) {
+        output = false;
+      }
+    }
+  }
+  return output;
+};
+const assertArraysEqual = function(arrayA, arrayB,) {
+  if (eqArrays(arrayA, arrayB) === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${arrayA} === ${arrayB}`);
+  } else if (eqArrays(arrayA, arrayB) === false) {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arrayA} !== ${arrayB} `);
+  }
+};
+
+
+const takeUntil = function(array, callback) {
+  const output = [];
+  
+  for (const item of array) {
+    if (callback(item)) {
+      return output;
+    } else {
+      output.push(item);
+    }
+  }
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood']);
+
+const data3 = [30, 20, 19, 39, 50, 51];
+assertArraysEqual(takeUntil(data3, x => x > 50), [30, 20, 19, 39, 50]);
