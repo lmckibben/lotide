9c10fcab68f2e0a9ebd2f48dbb3beed67e7c767d
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-23 18:57:13 +0000

    Modularize code

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const tail = function(array) {
   let tailArray = [];
   
@@ -17,45 +8,4 @@ const tail = function(array) {
   return tailArray;
 };
 
-//test 1
-let array = ["Light", "House", "Labs"];
-tail(array);
-assertEqual(array.length, 3);
-console.log(tail(array));
-let result = tail(array);
-assertEqual(result.length, 2);
-assertEqual(result[0], "House");
-assertEqual(result[1], "Labs");
-
-//test 2
-array = [1,2,3,4,5];
-tail(array);
-assertEqual(array.length, 5);
-console.log(tail(array));
-result = tail(array);
-assertEqual(result.length, 4);
-assertEqual(result[0], 2);
-assertEqual(result[1], 3);
-assertEqual(result[2], 4);
-assertEqual(result[3], 5);
-
-//test 3
-array = [1];
-tail(array);
-assertEqual(array.length, 1);
-console.log(tail(array));
-result = tail(array);
-assertEqual(result.length, 0);
-assertEqual(result[0], undefined);
-
-//test 4
-array = [1,2,3,4,5];
-tail(array);
-assertEqual(array.length, 5);
-console.log(tail(array));
-result = tail(array);
-assertEqual(result.length, 3);
-assertEqual(result[0], 1);
-assertEqual(result[1], 2);
-assertEqual(result[2], 3);
-assertEqual(result[3], 4);
+module.exports = tail;
