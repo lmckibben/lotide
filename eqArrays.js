57b57b49faf50b2885b921fa4709617092a6c1c5
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-23 19:10:10 +0000

    refactored code to be modular

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arrayA, arrayB) {
   let output = true;
 
@@ -22,9 +14,4 @@ const eqArrays = function(arrayA, arrayB) {
   return output;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2,]), true);
-/* assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), false);
-assertEqual(eqArrays([1, true, 3], [1, 2, 3]), false);
-assertEqual(eqArrays([1, NaN, 3], [1, 2, 3]), false); */
+module.exports = eqArrays;
