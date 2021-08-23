bbe8fed5c6b093393c46285467555c101662fb88
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-23 19:20:35 +0000

    refactored code to be modular

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,18 +1,5 @@
-const eqArrays = function(arrayA, arrayB) {
-  let output = true;
+const eqArrays = require('./eqArrays');
 
-  if (arrayA.length !== arrayB.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayA.length; i++) {
-    for (let j = 0; j < arrayB.length; j++) {
-      if (i === j && arrayA[i] !== arrayB[j]) {
-        output = false;
-      }
-    }
-  }
-  return output;
-};
 const assertArraysEqual = function(arrayA, arrayB,) {
   if (eqArrays(arrayA, arrayB) === true) {
     console.log(`🟢🟢🟢 Assertion Passed: ${arrayA} === ${arrayB}`);
@@ -21,7 +8,4 @@ const assertArraysEqual = function(arrayA, arrayB,) {
   }
 };
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,"2",3], [1,2,3]);
-assertArraysEqual([true,2,3], [true,2,3]);
-assertArraysEqual([false,2,3], [true,2,3]);
+module.exports = assertArraysEqual;
