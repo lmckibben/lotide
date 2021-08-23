33ced8bad1abf67a0c62cefc4048ea5b42253d43
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-23 19:11:32 +0000

    created tests for eqArrays.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2,]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, true, 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, NaN, 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, NaN, 3], [1, 2, 3]), false);
