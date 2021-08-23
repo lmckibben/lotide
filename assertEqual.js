3f2db4e1ec1cca362e9929b2713e5c2da7546a38
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-23 16:21:55 +0000

    modularizing code

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("This is a string", "This is a string");
-assertEqual("This is a String", "This is A string");
-assertEqual(10, 10.1);
-assertEqual(10, 10.0);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,18 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require(('./assertEqual'));
 
 const head = function(array) {
   let first = array[0];
   return first;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1,2,3]), 3);
-assertEqual(head(["This", "is", "a", "test"]), "a");
-assertEqual(head(["This", "is", "a", "test"]), "This");
+module.exports = head;
