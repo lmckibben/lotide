26208a867b3ae202b1e4c6cbe055196c7238760d
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-23 20:03:44 +0000

    conveted testing to use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,32 @@
-const assertEqual = require(('../assertEqual'));
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1,2,3]), 3);
-assertEqual(head(["This", "is", "a", "test"]), "a");
-assertEqual(head(["This", "is", "a", "test"]), "This");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse' 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns true for [1, 2, 3] !== 3", () => {
+    assert.notStrictEqual(head([1, 2, 3]), 3);
+  });
+
+  it("returns true for ['This', 'is', 'a', 'test'] !== 'a'", () => {
+    assert.notStrictEqual(head([1, 2, 3]), 3);
+  });
+
+  it("returns 'This' for ['This', 'is', 'a', 'test']", () => {
+    assert.notStrictEqual(head(["This", "is", "a", "test"]), "a");
+  });
+});
