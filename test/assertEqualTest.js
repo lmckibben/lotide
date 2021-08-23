45c7be6d319630f61b947261668b47fb41dce971
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-23 16:23:05 +0000

    created tests for assertEqual and head

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("This is a string", "This is a string");
+assertEqual("This is a String", "This is A string");
+assertEqual(10, 10.1);
+assertEqual(10, 10.0);
